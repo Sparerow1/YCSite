37695ea4a957ad90a8bf67a57b8a3646d43298ca
Sparerow1 <yc01jun6@gmail.com>
2025-08-25 22:39:37 -0400

    commenting experience tab for now

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,12 +1,10 @@
 import sqlite3 from "sqlite3";
 import { open, Database } from "sqlite";
-import { NextApiRequest, NextApiResponse } from 'next';
 
 // Initialize a variable to hold the SQLite database connection
-// we will initilize it as null, but we will assign the connection later on
 let db: Database | null = null;
 
-async function connectDatabase(res: NextApiResponse) {
+async function connectDatabase(): Promise<Database> {
     if (!db) {
         try {
             db = await open({
@@ -15,25 +13,27 @@ async function connectDatabase(res: NextApiResponse) {
             });
         } catch (error) {
             console.log("Error connecting to the database", error);
-            return res.status(500).send("Error connecting to the database");
+            throw error;
         }
     }
+    return db;
 }
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-    await connectDatabase(res);
-
-    const paragraphs = await db!.all(`
-        SELECT * FROM paragraphs 
-            join nodes on nodes.nodeId = paragraphs.nodeId`);
-    
+export async function GET() {
     try {
-            return new Response(JSON.stringify(paragraphs), {
-                headers: {
-                "Content-Type": "application/json"},
-                status: 200
-            });
-    } catch (error) {    
-        return res.status(500).send("Error fetching data from the database: " + error);
+        const dbConn = await connectDatabase();
+
+        const paragraphs = await dbConn.all(`
+            SELECT * FROM paragraphs 
+                JOIN nodes ON nodes.nodeId = paragraphs.nodeId
+        `);
+
+        return new Response(JSON.stringify(paragraphs), {
+            headers: { "Content-Type": "application/json" },
+            status: 200
+        });
+    } catch (error) {
+        console.error("Error fetching data from the database:", error);
+        return new Response("Error fetching data from the database", { status: 500 });
     }
 }
